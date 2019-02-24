a9ab7cb665ce6150d50147fd57b50e290a0a0831
John Thornton <jthornton@gnipsel.com>
2019-02-24 11:45:03 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mspindle",
+    version="0.0.1",
+    author="John Doe",
+    author_email="<doe.john@example.com>",
+    description="BP_Mill - A QtPyVCP based Virtual Control Panel for LinuxCNC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/USERNAME/REPO",
+    download_url="https://github.com/USERNAME/REPO/tarball/master",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'gui_scripts': [
+            'mspindle=mspindle:main',
+        ],
+        'qtpyvcp.vcp': [
+            'mspindle=mspindle',
+        ],
+    },
+    install_requires=[
+       'qtpyvcp>=0.1.4',
+    ],
+)
